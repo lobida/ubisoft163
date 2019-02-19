f96d5cd0c9590dc48a760476a505ca00b1866108
Jay Wang <jaywanglevelup@yahoo.com>
2019-02-19 16:53:36 +0800

    add lambda permission for s3 bucket

--- lambda_functions/main.tf
+++ lambda_functions/main.tf
@@ -4,10 +4,17 @@ data "archive_file" "lambda_zip" {
     output_path   = "lambda_function.zip"
 }
 resource "aws_lambda_function" "bucket_notification_lambda" {
-  filename         = "lambda_function"
+  filename         = "lambda_function.zip"
   function_name    = "bucket_notification_lambda"
   role             = "${var.lambda_role}"
   handler          = "echo.bucket_notification"
   source_code_hash = "${base64sha256(file("lambda_function.zip"))}"
   runtime          = "python3.6"
+}
+
+resource "aws_lambda_permission" "allow_bucket_notification" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.bucket_notification_lambda.function_name}"
+  principal     = "s3.amazonaws.com"
 }
