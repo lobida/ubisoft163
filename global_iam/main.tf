40d7d6ea3deb540a7344dda203bfc1bc21293f63
Jay Wang <jaywanglevelup@yahoo.com>
2019-02-20 09:47:35 +0800

    change event type to delete

--- global_iam/main.tf
+++ global_iam/main.tf
@@ -18,10 +18,42 @@ resource "aws_iam_role" "lambda_iam" {
 EOF
 }
 
+resource "aws_iam_policy" "lambda_access_db_policy" {
+  name        = "lambda_access_db_policy"
+  path        = "/"
+  description = "Lambda to read/write to dynamodb"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:BatchGetItem",
+				"dynamodb:GetItem",
+				"dynamodb:Query",
+				"dynamodb:Scan",
+				"dynamodb:BatchWriteItem",
+				"dynamodb:PutItem",
+				"dynamodb:UpdateItem"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
 data "aws_iam_policy" "AWSLambdaBasicExecutionRole" {
   arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 resource "aws_iam_role_policy_attachment" "lambda_basic_role_attach" {
   role       = "${aws_iam_role.lambda_iam.name}"
   policy_arn = "${data.aws_iam_policy.AWSLambdaBasicExecutionRole.arn}"
+}
+resource "aws_iam_role_policy_attachment" "lambda_db_access_policy_attach" {
+  role       = "${aws_iam_role.lambda_iam.name}"
+  policy_arn = "${aws_iam_policy.lambda_access_db_policy.arn}"
 }
--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -17,6 +17,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
   lambda_function {
     lambda_function_arn = "${var.bucket_notification_lambda_arn}"
-    events              = ["s3:ObjectCreated:*"]
+    events              = ["s3:ObjectRemoved:Delete"]
   }
 }
